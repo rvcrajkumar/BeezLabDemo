cab23011e2e1d372e8a78819fd403c59974c34b9
rvcrajkumar <rvc.rajkumar@gmail.com>
2024-03-20 16:12:22 +0530

    ver 1
    ver 1

---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            AppDomain subsidiaryDomain = AppDomain.CreateDomain("SubsidiaryDomain");
+
+            try
+            {
+               
+                Assembly assembly = subsidiaryDomain.Load("SubsidiaryCodeDLL");
+
+              
+                Type type = assembly.GetType("SubsidiaryCodeDLL.SubsidiaryCodeClass");
+                dynamic instance = Activator.CreateInstance(type);
+                Console.WriteLine("Enter any Integer number: ");
+                try
+                {
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Data Sent to SubsidiaryCodeDLL is: " + input.ToString());
+                    string result = instance.ProcessData(input);
+
+
+                    Console.WriteLine("Response from SubsidiaryCodeDLL:");
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Please enter input number: " + ex.Message);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error executing subsidiary code: " + ex.Message);
+            }
+            finally
+            {
+                
+                AppDomain.Unload(subsidiaryDomain);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SubsidiaryCodeDLL/SubsidiaryCodeDLL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsidiaryCodeDLL
+{
+    public class SubsidiaryCodeClass : MarshalByRefObject
+    {
+        public string ProcessData(int input)
+        {
+            // Example: Add custom logic here to process the input
+            return $"Received: {input.ToString()}, Processed by SubsidiaryCodeDLL is : {(input*2).ToString()} .";
+        }
+    }
+}
